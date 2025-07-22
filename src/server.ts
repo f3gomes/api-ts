fc2f8135b0f215f212f01ed5c2bbde4eaae08e46
f3gomes <fgomesdeluna@gmail.com>
2025-07-22 10:45:50 -0300

    build: dotenv config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
-import config from './config';
+import config from '@/config';
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello API',
+  });
+});
 
 app.listen(config.PORT, () => {
   console.log(`Server is running on port http://localhost:${config.PORT}`);
