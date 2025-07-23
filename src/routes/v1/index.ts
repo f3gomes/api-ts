c7b6eebc718746728a03a76851447c9369922fae
f3gomes <fgomesdeluna@gmail.com>
2025-07-23 15:53:32 -0300

    feat: create auth middlewares

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.status(200).json({
+  res.status(StatusCodes.OK).json({
     message: 'API is online',
     status: 'ok',
     version: '1.0.0',
