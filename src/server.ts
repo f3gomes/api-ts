c3329b04fe40590c788ce2eca1eb4724fb84012c
f3gomes <fgomesdeluna@gmail.com>
2025-07-23 09:34:53 -0300

    feat: create test route v1

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ import corsOptions from './config/cors';
 import config from '@/config';
 import limiter from '@/lib/express-rate-limit';
 
+import v1Routes from '@/routes/v1';
+
 const app = express();
 
 app.use(cors(corsOptions));
@@ -24,12 +26,18 @@ app.use(
 app.use(helmet());
 app.use(limiter);
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello API',
-  });
-});
+(async () => {
+  try {
+    app.use('/api/v1', v1Routes);
 
-app.listen(config.PORT, () => {
-  console.log(`Server is running on port http://localhost:${config.PORT}`);
-});
+    app.listen(config.PORT, () => {
+      console.log(`Server is running on port http://localhost:${config.PORT}`);
+    });
+  } catch (error) {
+    console.log('Failed to start the server', error);
+
+    if (config.NODE_ENV === 'production') {
+      process.exit(1);
+    }
+  }
+})();
